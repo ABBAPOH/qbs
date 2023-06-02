62e8aa33c573666531dfbba1ff563fdb0f70818c
Christian Kandeler <christian.kandeler@qt.io>
2023-06-02 15:39:55 +0200

    Update dmgbuild
    Change-Id: Ia9eefa977a5148a9b5aa2e6a39087a7c55618d49

--- tests/auto/blackbox/tst_blackboxapple.cpp
+++ tests/auto/blackbox/tst_blackboxapple.cpp
@@ -890,8 +890,6 @@ void TestBlackboxApple::deploymentTarget_data()
 
 void TestBlackboxApple::dmg()
 {
-    if (qEnvironmentVariableIsSet("GITHUB_ACTIONS"))
-        QSKIP("Skip this test when running on GitHub");
     QDir::setCurrent(testDataDir + "/apple-dmg");
     QCOMPARE(runQbs(), 0);
 }
