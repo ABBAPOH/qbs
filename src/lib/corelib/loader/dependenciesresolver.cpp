7a22771844cc318c89733498b91b7cc5a7116dc3
Ivan Komissarov <abbapoh@gmail.com>
2025-05-25 15:51:34 +0300

    wip. quickjs: update to v0.10.1
    Change-Id: I3b01f57328c540521c1ee5572891a91658e7d24f

--- src/lib/corelib/loader/dependenciesresolver.cpp
+++ src/lib/corelib/loader/dependenciesresolver.cpp
@@ -1124,7 +1124,7 @@ QVariantMap safeToVariant(JSContext *ctx, const JSValue &v)
         if (JS_IsError(ctx, u))
             throw ErrorInfo(getJsString(ctx, u));
         const QString name = getJsString(ctx, prop);
-        result[name] = (JS_IsObject(u) && !JS_IsArray(ctx, u) && !JS_IsRegExp(u))
+        result[name] = (JS_IsObject(u) && !JS_IsArray(u) && !JS_IsRegExp(u))
                            ? safeToVariant(ctx, u)
                            : getJsVariant(ctx, u);
     });
